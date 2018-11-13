1e9ccab9f5b3012befc21026504b6c95e0d8df82
MykytaFox <naktasglu@gmail.com>
2018-11-13 19:22:59 +0200

    print number

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for i  in 0..<5 {
+            print ("number =",i)
+        }
     }
 
 
