f1e707c0f55cbe5b33873091dd6d394b12348a02
MykytaFox <naktasglu@gmail.com>
2018-11-13 18:14:47 +0200

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Mykyta Kvashuk on 11/13/18.
+//  Copyright © 2018 Mykyta Kvashuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
